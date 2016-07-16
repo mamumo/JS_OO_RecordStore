a97492660bd29878863702e401be47a01c345818
Marie Moyles <marie.moyles2@mail.dcu.ie>
2016-07-16 10:14:04 +0100

    list inventory completed

--- record_store.js
+++ record_store.js
@@ -1,11 +1,23 @@
 var _ = require('lodash')
 
-var RecordStore = function(name, city) {
+var RecordStore = function(name, city, cashInBank) {
   this.name = name,
   this.city = city,
+  this.cashInBank = cashInBank
   this.inventory = []
 }
 
+RecordStore.prototype = {
+  addRecord: function(record) {
+    this.inventory.push(record);
+  },
 
+  listsInventory: function() {
+    var inventoryList = this.inventory.map(function(record) {
+      return inventoryList});
+  }
+
+}
+  
 
 module.exports = RecordStore;
--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -1,20 +1,57 @@
 var RecordStore = require('../record_store');
+var Record = require('../record');
 var assert = require('chai').assert
 
 describe('Record Store', function() {
 
   it('should have a name', function(){
-    recordStore1 = new RecordStore("Tommys Tunes", "Glasgow")
+    var recordStore1 = new RecordStore("Tommys Tunes", "Glasgow", 100)
     assert.equal("Tommys Tunes", recordStore1.name)
   })
 
   it('should have a city', function(){
-    recordStore1 = new RecordStore("Tommys Tunes", "Glasgow")
+    var recordStore1 = new RecordStore("Tommys Tunes", "Glasgow")
     assert.equal("Glasgow", recordStore1.city)
   })
 
   it('should have 0 records in inventory to start', function(){
-    recordStore1 = new RecordStore("Tommys Tunes", "Glasgow")
+    var recordStore1 = new RecordStore("Tommys Tunes", "Glasgow")
     assert.equal( 0, recordStore1.inventory.length)
   })
+
+  it('should have opening balance', function(){
+    var recordStore1 = new RecordStore("Tommys Tunes", "Glasgow", 100)
+    assert.equal( 100, recordStore1.cashInBank)
+  })
+
+  it('should be able to add records to store', function(){
+    var recordStore1 = new RecordStore("Tommys Tunes", "Glasgow");
+    var record1 = new Record("Michael Jackson", "Billie Jean", 5);
+    var record2 = new Record("Abba", "Knowing Me Knowing You", 10);
+    var record3 = new Record("Tom Jones", "Delilah", 6);
+    var record4 = new Record("Now 64", "Various", 20);
+    recordStore1.addRecord(record1);
+    recordStore1.addRecord(record2);
+    recordStore1.addRecord(record3);
+    recordStore1.addRecord(record4);
+    assert.equal( 4, recordStore1.inventory.length)
+  })
+
+  it('should be able to list inventory', function(){
+    var recordStore1 = new RecordStore("Tommys Tunes", "Glasgow", 100);
+    var record1 = new Record("Michael Jackson", "Billie Jean", 5);
+    var record2 = new Record("Abba", "Knowing Me Knowing You", 10);
+    var record3 = new Record("Tom Jones", "Delilah", 6);
+    var record4 = new Record("Now 64", "Various", 20);
+    recordStore1.addRecord(record1);
+    recordStore1.addRecord(record2);
+    recordStore1.addRecord(record3);
+    recordStore1.addRecord(record4);
+    recordStore1.listInventory;
+    assert.deepEqual( [record1, record2, record3, record4], recordStore1.inventory)
+  })
+
+  it 
+
+
 })
